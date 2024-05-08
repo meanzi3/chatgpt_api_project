1dd85994c45173f598700d5b0b7c157ca3aac94f
meanzi3 <minjichoe0326@gmail.com>
2024-05-08 18:47:11 +0900

    init project : 프로젝트 세팅
    - chatGPT API를 연동하기 위한 프로젝트 설정 초기화
    - properties 파일에 model, url, key 저장

---
+++ src/main/java/com/example/chatgpt_api/ChatgptApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.chatgpt_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatgptApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatgptApiApplication.class, args);
+	}
+
+}
